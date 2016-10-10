ef482909e7bea9cb099dabe05fb274202558798c
Alangi Derick <alangiderick@gmail.com>
2016-10-10 21:45:16 +0100

    Quick Sort Algorithm
    * Implemented the Quick sort algorithm data structure in
      GoLang which solves the #6 issue on the AlGo repo on
      Github.
    
    * The algorithm has test cases including testpairs that
      can be runned to check if everything is sorted in the
      right order.
    
    * Also, the reflect package was used to check for equality
      of slices in GoLang and the math/rand for random pivot
      selection in the Slice.
    
    * Formated the PR
    
    Signed-off-by: Alangi Derick <alangiderick@gmail.com>

---
+++ qsort/qsort_test.go
@@ -0,0 +1,36 @@
+/* Testing the Qsort() function in the qsort package */
+/* Author: Alangi Derick (@d3r1ck), Date: 10/10/2016 */
+
+package qsort
+
+import (
+	"reflect"
+	"testing"
+)
+
+/* Test pair for Qsort() algorithm */
+type testpair struct {
+	unsorted_slice []int
+	sorted_slice   []int
+}
+
+/* Test instance of testpair the unsorted and sorted slices */
+var tests = []testpair{
+	{[]int{3, 2, 1, 5, 6, 3, 7, 8, 9}, []int{1, 2, 3, 3, 5, 6, 7, 8, 9}},
+	{[]int{1, 4, 2, 3}, []int{1, 2, 3, 4}},
+}
+
+/* Test case to test the Qsort function */
+func TestQsort(t *testing.T) {
+	for _, pair := range tests {
+		sorted := Qsort(pair.unsorted_slice)
+
+		if !reflect.DeepEqual(sorted, pair.sorted_slice) {
+			t.Error(
+				"For", pair.unsorted_slice,
+				"expected", pair.sorted_slice,
+				"got", sorted,
+			)
+		}
+	}
+}
