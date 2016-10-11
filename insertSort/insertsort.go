2dc3a363aa8a9f9174540f7fd0f718815bf44d07
Mvkete <mukete@yandex.com>
2016-10-11 08:04:32 +0100

    Insert sort & insert sort tests insert sort package and the required test to perform simple basic array sorting in go lang #23

--- insertSort/insertsort.go
+++ insertSort/insertsort.go
@@ -6,7 +6,7 @@ func Swap(arraysrt []int, i, j int) {
 	arraysrt[i] = tmp
 }
 
-func insertSort(array []int) {
+func InsertSort(array []int) {
 	for i := 1; i < len(array); i++ {
 		for j := i; j > 0 && array[j] < array[j-1]; j-- {
 			Swap(array, j, j-1)
