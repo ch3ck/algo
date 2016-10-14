be46fd341fd50e91615a47bd8003e0fd51fc8a3e
Mvkete <mukete@yandex.com>
2016-10-14 22:51:38 +0100

    Rabin Karp sorting algo with tests Simple rabin karp algo, with sample tests fix for issue #20 👍🏾

---
+++ rabinkarp/rabinkarp_test.go
@@ -0,0 +1,42 @@
+
+package rabinkarp
+
+import (
+	"reflect"
+	"sort"
+	"testing"
+)
+
+// Struct for test data
+var SearchTests = []struct {
+	txt      string
+	patterns []string
+	expected []string
+}{
+	{"walter h white", []string{"white", "pinkman", "walter"}, []string{"walter", "white"}},
+	{"damon salvatore", []string{"damon", "stephen", "salvatore"}, []string{"damon", "salvatore"}},
+}
+
+// run testcase
+func TestSearch(t *testing.T) {
+	for _, ct := range SearchTests {
+		found := Search(ct.txt, ct.patterns)
+		if !eq(found, ct.expected) {
+			t.Errorf("Broken test .....",
+				ct.txt, ct.patterns, found, ct.expected)
+		}
+	}
+}
+// sort string for comparison
+func eq(f, s []string) bool {
+	fx := make([]string, len(f))
+	sx := make([]string, len(s))
+	copy(fx, f)
+	copy(sx, s)
+
+	sort.Strings(fx)
+	sort.Strings(sx)
+	return reflect.DeepEqual(fx, sx)
+}
+
+
