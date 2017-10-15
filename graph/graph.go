483ed6154c0d2e71df9975af57fc0bbd8d63e379
Keith Kim <keith.karmakaze@gmail.com>
2017-10-15 00:07:41 -0400

    Directed Graph and Depth First Search in Golang #14

--- graph/graph.go
+++ graph/graph.go
@@ -35,6 +35,7 @@ func (n node) Edges() []ID {
 type Graph interface {
 	AddNode(id ID, edges []ID) Node
 	BreadthFirstTraversal(start ID, visitor NodeVisitorFunc) error
+	DepthFirstTraversal(start ID, visitor NodeVisitorFunc) error
 }
 
 // returning an error terminates the traversal
@@ -59,7 +60,7 @@ func (g graph) AddNode(id ID, edges []ID) Node {
 	return node
 }
 
-// Visits each node in breadth-first order. if the visitor function returns and error
+// Visits each node in breadth-first order. if the visitor function returns an error
 // traversal is terminated and the error returned by this function.
 func (g graph) BreadthFirstTraversal(start ID, visitor NodeVisitorFunc) error {
 	toVisitIds := []ID{}
@@ -88,5 +89,34 @@ func (g graph) BreadthFirstTraversal(start ID, visitor NodeVisitorFunc) error {
 		toVisitIds = toVisitIds[1:]
 	}
 
+	return nil
+}
+
+// Visits each node in depth-first order. if the visitor function returns an error
+// traversal is terminated and the error returned by this function.
+func (g graph) DepthFirstTraversal(start ID, visitor NodeVisitorFunc) error {
+	visitedIds := map[ID]interface{}{}
+
+	return g.depthFirstTraversal(start, 0, visitedIds, visitor)
+}
+
+func (g graph) depthFirstTraversal(currId ID, depth int, visitedIds map[ID]interface{}, visitor NodeVisitorFunc) error {
+	curr := g.nodes[currId]
+	if curr == nil {
+		return fmt.Errorf("inconsistent graph or start, missing node with id %s", currId)
+	}
+
+	if err := visitor(curr, depth); err != nil {
+		return err
+	}
+	visitedIds[currId] = nil
+
+	for _, id := range curr.edges {
+		if _, found := visitedIds[id]; !found {
+			if err := g.depthFirstTraversal(id, depth + 1, visitedIds, visitor); err != nil {
+				return err
+			}
+		}
+	}
 	return nil
 }
--- graph/graph_test.go
+++ graph/graph_test.go
@@ -8,7 +8,7 @@ import (
 )
 
 func TestBreadthFirstTraversal(t *testing.T) {
-	t.Run("directed graph depth first traversal", func(t *testing.T) {
+	t.Run("directed graph breadth first traversal", func(t *testing.T) {
 		// create data
 		g := graph.NewDirectedGraph()
 		g.AddNode("0", []graph.ID{"1a", "1b", "1c"})
@@ -26,6 +26,34 @@ func TestBreadthFirstTraversal(t *testing.T) {
 		expected := []graph.ID{"0", "1a", "1b", "1c", "2a1", "2b1", "2b2", "2c1", "3a1"}
 		actual := []graph.ID{}
 		err := g.BreadthFirstTraversal("0", func(n graph.Node, depth int) error {
+			actual = append(actual, n.Id())
+			return nil
+		})
+
+		assert.NoError(t, err, "traversal error")
+		assert.Equal(t, expected, actual, "traversal order failed")
+	})
+}
+
+func TestDepthFirstTraversal(t *testing.T) {
+	t.Run("directed graph depth first traversal", func(t *testing.T) {
+		// create data
+		g := graph.NewDirectedGraph()
+		g.AddNode("0", []graph.ID{"1a", "1b", "1c"})
+		g.AddNode("1a", []graph.ID{"2a1"})
+		g.AddNode("1b", []graph.ID{"2b1", "2b2"})
+		g.AddNode("1c", []graph.ID{"2c1"})
+		g.AddNode("2a1", []graph.ID{"3a1"})
+		g.AddNode("2b1", []graph.ID{"0", "1c"})
+		g.AddNode("2b2", []graph.ID{"1a"})
+		g.AddNode("2c1", []graph.ID{"2c1"})
+		g.AddNode("3a1", []graph.ID{})
+
+		fmt.Printf("input: %+v\n", g)
+
+		expected := []graph.ID{"0", "1a", "2a1", "3a1", "1b", "2b1", "1c", "2c1", "2b2"}
+		actual := []graph.ID{}
+		err := g.DepthFirstTraversal("0", func(n graph.Node, depth int) error {
 			actual = append(actual, n.Id())
 			return nil
 		})
