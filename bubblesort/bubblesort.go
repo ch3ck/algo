776fccf52c2399103fe2f7b9af9d4df1793f29ca
Breno Helfstein Moura <breno.moura@hotmail.com>
2017-10-25 17:30:31 -0200

    Implementation of bubblesort (#59)

---
+++ bubblesort/bubblesort.go
@@ -0,0 +1,30 @@
+// Package bubblesort provides implementation for bubble sort algorithm
+package bubblesort
+
+// Swap function changes two integers
+func Swap(lhs *int, rhs *int) {
+	tmp := *lhs
+	*lhs = *rhs
+	*rhs = tmp
+}
+
+// Bubblesort function receives an array and return the sorted array
+// O(N^2)
+func bubbleSort(A []int) []int {
+	N := len(A)
+	result := make([]int, len(A));
+	i := 0
+	j := 0
+	for i = 0; i < N; i++ {
+		result[i] = A[i];
+	}
+
+	for i = 0; i < N - 1; i++ {
+		for j = i; j < N - 1; j++ {
+			if (result[j] > result[j + 1]) {
+				Swap(&result[j], &result[j + 1])
+			}
+		}
+	}
+	return result
+}
---
+++ bubblesort/bubblesort_test.go
@@ -0,0 +1,47 @@
+package bubblesort
+
+import (
+	"math/rand"
+	"reflect"
+	"sort"
+	"testing"
+	"time"
+)
+
+// TestBubbleSort inspired in mergesort_test
+func TestBubbleSort(t *testing.T) {
+	empty := []int{}
+	resultEmpty := bubbleSort(empty)
+
+	if !reflect.DeepEqual(empty, resultEmpty) {
+		t.Error("ERROR!", "Expected:", empty, "Result:", resultEmpty)
+		return
+	}
+
+	unitary := []int{42}
+	resultUnitary := bubbleSort(unitary)
+
+	if !reflect.DeepEqual(unitary, resultUnitary) {
+		t.Error("ERROR!", "Expected:", unitary, "Result:", resultUnitary)
+		return
+	}
+
+	rand.Seed(int64(time.Now().Nanosecond()))
+	for test := 0; test < 10; test++ {
+		array := make([]int, 100)
+		for i := 0; i < 100; i++ {
+			array[i] = rand.Int()
+		}
+
+		expected := make([]int, 100)
+		copy(expected, array)
+		sort.Ints(expected)
+
+		result := bubbleSort(array)
+
+		if !reflect.DeepEqual(result, expected) {
+			t.Error("ERROR!", "Expected:", expected, "Result:", result)
+			return
+		}
+	}
+}
