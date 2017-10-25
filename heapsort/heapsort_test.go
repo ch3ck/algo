9d594c2c93d80bb2fa4a57e6d52fac688e67f15f
Arthur Silva <arxdsilva@gmail.com>
2017-10-25 17:30:11 -0200

    fix gofmt issues (#62)
    * fix gofmt issues
    
    * fix tests
    
    * fix imports

--- heapsort/heapsort_test.go
+++ heapsort/heapsort_test.go
@@ -1,43 +1,43 @@
 package heapsort_test
 
 import (
-    "testing"
-    "github.com/lborg019/Algo/heapsort"
-    "fmt"
+	"fmt"
+	"testing"
+
+	"github.com/Ch3ck/AlGo/heapsort"
 )
 
-func TestHeapsort(t *testing.T){
-    var data = []int{3,1,4,1,5,9,2,6} //create data
-    fmt.Println("input:  ",data)
-    var expected = []int{1, 1, 2, 3, 4, 5, 6, 9}
-    var result, flag = heapsort.Heapsort(data, len(data))
+func TestHeapsort(t *testing.T) {
+	var data = []int{3, 1, 4, 1, 5, 9, 2, 6} //create data
+	fmt.Println("input:  ", data)
+	var expected = []int{1, 1, 2, 3, 4, 5, 6, 9}
+	var result, flag = heapsort.Heapsort(data, len(data))
 
-    if flag != 1{
-        fmt.Print("Something went wrong")
-        return
-    }
+	if flag != 1 {
+		fmt.Print("Something went wrong")
+		return
+	}
 
-    //check if result is null
-    if result == nil{
-        t.Error("returned value is nil")
-        return
-    }
+	//check if result is null
+	if result == nil {
+		t.Error("returned value is nil")
+		return
+	}
 
-    //result is not null, we perform checks
-    if len(result) != len(expected){
-        t.Error("sorted data has different length from input data")
-        return
-    } else{ //lengths are okay, we compare output values with expected values
-            for i := range result{
-                if result[i] != expected[i]{
-                    t.Error("failed at sorting")
-                } else
-                {
-                    fmt.Println("expect: ",expected)
-                    fmt.Println("output: ", result)
-                    fmt.Println("success")
-                    return
-                }
-            }
-    }
+	//result is not null, we perform checks
+	if len(result) != len(expected) {
+		t.Error("sorted data has different length from input data")
+		return
+	} else { //lengths are okay, we compare output values with expected values
+		for i := range result {
+			if result[i] != expected[i] {
+				t.Error("failed at sorting")
+			} else {
+				fmt.Println("expect: ", expected)
+				fmt.Println("output: ", result)
+				fmt.Println("success")
+				return
+			}
+		}
+	}
 }
--- qsort/qsort_test.go
+++ qsort/qsort_test.go
@@ -16,16 +16,21 @@ type testpair struct {
 
 /* Test instance of testpair the unsorted and sorted slices */
 var tests = []testpair{
-	{[]int{3, 2, 1, 5, 6, 3, 7, 8, 9}, []int{1, 2, 3, 3, 5, 6, 7, 8, 9}},
-	{[]int{1, 4, 2, 3}, []int{1, 2, 3, 4}},
+	{
+		unsortedSlice: []int{3, 2, 1, 5, 6, 3, 7, 8, 9},
+		sortedSlice:   []int{1, 2, 3, 3, 5, 6, 7, 8, 9},
+	},
+	{
+		unsortedSlice: []int{1, 4, 2, 3},
+		sortedSlice:   []int{1, 2, 3, 4}},
 }
 
 /* Test case to test the Qsort function */
 func TestQsort(t *testing.T) {
 	for _, pair := range tests {
-		sorted := Qsort(pair.unsorted_slice)
+		sorted := Qsort(pair.unsortedSlice)
 
-		if !reflect.DeepEqual(sorted, pair.sorted_slice) {
+		if !reflect.DeepEqual(sorted, pair.sortedSlice) {
 			t.Error(
 				"For", pair.unsortedSlice,
 				"expected", pair.sortedSlice,
