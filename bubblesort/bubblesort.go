7ae0927a50609325a07a1265ece70c2a48c3fcb9
A. Anabila Abeo <anthonyabeo@gmail.com>
2018-10-19 17:06:25 +0000

    fix bubble sort error

--- bubblesort/bubblesort.go
+++ bubblesort/bubblesort.go
@@ -12,16 +12,16 @@ func Swap(lhs *int, rhs *int) {
 // O(N^2)
 func bubbleSort(A []int) []int {
 	N := len(A)
-	result := make([]int, len(A));
+	result := make([]int, len(A))
 	i := 0
 	j := 0
 	for i = 0; i < N; i++ {
-		result[i] = A[i];
+		result[i] = A[i]
 	}
 
 	for i = 0; i < N - 1; i++ {
-		for j = i; j < N - 1; j++ {
-			if (result[j] > result[j + 1]) {
+		for j = 0; j < N - 1; j++ {
+			if result[j] > result[j + 1] {
 				Swap(&result[j], &result[j + 1])
 			}
 		}
